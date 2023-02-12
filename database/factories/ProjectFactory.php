9a102dedd5a680d356a6d69d52cc1e9a89d1bf60
LucaVitale00 <luca16vitale2000@gmail.com>
2023-02-12 16:20:55 +0100

    model, factory and seeder

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake() -> unique() -> words(7, true) ,
+            'description' => fake() -> boolean() 
+                                        ? fake() -> words(20, true)
+                                        : ""  ,
+            'main_image' => fake() -> unique() -> imageUrl(640, 480, 'animals', true)  ,
+            'relase_date' => fake() -> date()  ,
+            'repo_link' => fake() -> unique() -> words(10, true)  ,
+        ];
+    }
+}
